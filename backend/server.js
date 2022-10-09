5d06d70abd5958f83b3b0914ed56b9f06247efec
Bernadette Christiner <b.christiner93@gmail.com>
2022-10-09 19:33:48 +0200

    Implement get request to send index file

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,13 @@ const app = express();
 const port = 9000;
 // app.listen(port);
 // console.log("http://localhost:" + port);
+const path = require("path");
 
 app.use(express.json()); //is a built in middleware function in Express. It parses incoming JSON requests and puts the parsed data in req.body
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+});
 
 app.listen(port);
 console.log("http://localhost:" + port);
