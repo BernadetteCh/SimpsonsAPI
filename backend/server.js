4401fba97683ea7cb5cc85776fa0cde8fb97f27c
Bernadette Christiner <b.christiner93@gmail.com>
2022-10-09 19:40:25 +0200

    Add comments

--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,13 @@ const app = express();
 const port = 9000;
 // app.listen(port);
 // console.log("http://localhost:" + port);
-const path = require("path");
+const path = require("path"); //The Path module provides a way of working with directories and file paths.
 
 app.use(express.json()); //is a built in middleware function in Express. It parses incoming JSON requests and puts the parsed data in req.body
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+  //dirname() method returns the directories of a file path.
 });
 
 app.listen(port);
