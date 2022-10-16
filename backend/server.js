6eff0a18474557a6ad5aaf923fa7e24925104834
Bernadette Christiner <b.christiner93@gmail.com>
2022-10-16 16:01:21 +0200

    create html files

--- backend/server.js
+++ backend/server.js
@@ -7,10 +7,27 @@ const port = 9000;
 const path = require("path"); //The Path module provides a way of working with directories and file paths.
 
 app.use(express.json()); //is a built in middleware function in Express. It parses incoming JSON requests and puts the parsed data in req.body
+app.use("/styles.css", express.static(`${__dirname}/../frontend/styles.css`)); // Static means pre-rendered web pages that do not change on time. Dynamic means it is generated in real-time at the time of the request by the server.//static files da veränder sich der content nicht, daher "nur" das css als static hier
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
   //dirname() method returns the directories of a file path.
+});
+app.get("/index.html", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+  //dirname() method returns the directories of a file path.
+});
+
+app.get("/choose-your-character", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/chooseYourCharacter.html`));
+});
+
+app.get("/built-your-character", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/chooseYourCharacter.html`));
+});
+
+app.get("/your-characters", (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/yourCharacters.html`));
 });
 
 app.listen(port);
