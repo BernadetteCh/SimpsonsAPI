ef8e40bf1bf1145120970722ee8a313e3a4bd745
Bernadette Christiner <b.christiner93@gmail.com>
2022-10-22 23:46:57 +0200

    Create fetch with post method to send data to server

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,6 @@
 url = "http://localhost:9000";
+// let userObj = [];
+let selectedUserCharacters = [];
 
 const imgSection = document.getElementById("simpsons-characters-img");
 // let simpsons;
@@ -24,18 +26,74 @@ function renderElements(data) {
 }
 function addEventListener() {
   [...document.getElementsByClassName("simpsons-family-img")].map((image) => {
-    image.addEventListener("click", () => {
-      alert("hello World");
-    });
+    image.addEventListener("click", sendSelectedCharacterToServer);
   });
 }
+
+async function sendSelectedCharacterToServer(event) {
+  let selectedImg = event.target.parentElement.firstChild.src;
+  let selectedName = event.target.parentElement.lastChild.innerHTML;
+  //Create Post Request to send it to the server:
+
+  await fetch("http://localhost:9000/api", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      img: selectedImg,
+      name: selectedName,
+    }),
+  });
+  // let hey = await si.json();
+  // console.log(hey);
+  // console.log("Si" + si.json());
+}
+
+async function getData() {
+  const response = await fetch("http://localhost:9000/api", {
+    method: "GET",
+  });
+
+  const obj = await response.json();
+  // selectedUserCharacters.push(obj);
+  return obj;
+}
+
+function displaySelectedCharacterFromUser(getDataFromUser) {
+  console.log(getDataFromUser);
+  let imgOfSelectedCharacter = document.getElementById("selected-img");
+  let nameOfSelectedCharacter = document.getElementById("selected-name");
+
+  getDataFromUser.userData.forEach(function (element) {
+    let img = document.createElement("img");
+    img.setAttribute("src", `${element.img}`);
+    img.setAttribute("class", "simpsons-family-img");
+    imgOfSelectedCharacter.appendChild(img);
+    let name = document.createElement("span");
+    name.innerHTML = element.name;
+    nameOfSelectedCharacter.appendChild(name);
+  });
+}
+
 async function loadPage() {
   const data = await fetchApiData();
-  renderElements(data);
-  addEventListener();
+  console.log(window.location);
+  if (
+    window.location.href === "http://localhost:9000/choose-your-character" ||
+    window.location.href ===
+      "http://localhost:9000/built-your-character#important"
+  ) {
+    renderElements(data);
+    addEventListener(); //sendPostRequest
+  }
+
+  const selectedCharacterFromUser = await getData();
+  displaySelectedCharacterFromUser(selectedCharacterFromUser);
 }
 
 loadPage();
+
 //1. Methode _-----------------------------------------------------
 // async function fetchApi() {
 //   const response = await fetch(url + "/api/data");
